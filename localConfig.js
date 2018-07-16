bd45623cd8fae921d3d9dbc039054fe693ba0b63
adri4silva <adri4silva@gmail.com>
2018-07-16 18:40:21 +0200

    Make mongoDB location static

--- localConfig.js
+++ localConfig.js
@@ -2,5 +2,6 @@ module.exports = {
 	jwt: {
 		secret: '-.242da-f,214,12-mfcasDSFSAF$"!GA$&$(&·%!",safasñfaG!&$%"453fASHP¨SAwreqwreqwfkadlsñf´dasklfhoqio1`543io5fjksa',
 		expiresIn: '3d'
-	}
+	},
+	mongoLocation: 'mongodb://localhost:27017/nodepop'
 };
